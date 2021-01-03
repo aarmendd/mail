5a1b693d272613493dfe5479869db96afdffdd20
Armendd <ahmeti.armend@gmail.com>
2021-01-03 03:15:59 +0100

    :fire:Static header and styling

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
-import React from 'react';
-import './App.css';
+import React from "react";
+import "./App.css";
+import Header from "./Header";
 
 function App() {
   return (
     <div className="app">
-      <h2>asdasd</h2>
+      <Header />
     </div>
   );
 }
