762091f363c3dbc2768085baa8a89d0cd2c53bc7
Armendd <ahmeti.armend@gmail.com>
2021-01-04 02:53:45 +0100

    :bookmark: EmailList Static and Styling

--- src/EmailList.js
+++ src/EmailList.js
@@ -6,8 +6,13 @@ import ChevronLeftIcon from "@material-ui/icons/ChevronLeft";
 import ChevronRightIcon from "@material-ui/icons/ChevronRight";
 import KeyboardHideIcon from "@material-ui/icons/KeyboardHide";
 import SettingsIcon from "@material-ui/icons/Settings";
+import InboxIcon from "@material-ui/icons/Inbox";
+import PeopleIcon from "@material-ui/icons/People";
+import LocalOfferIcon from "@material-ui/icons/LocalOffer";
 import React from "react";
 import "./EmailList.css";
+import Section from "./Section";
+import EmailRow from "./EmailRow";
 
 function EmailList() {
   return (
@@ -48,7 +53,24 @@ function EmailList() {
       </div>
 
       <div className="emailList__sections">
-          
+        <Section Icon={InboxIcon} title="Primary" color="red" selected />
+        <Section Icon={PeopleIcon} title="Social" color="blue" />
+        <Section Icon={LocalOfferIcon} title="Promotions" color="green" />
+      </div>
+
+      <div className="emailList__list">
+        <EmailRow
+          title="Title"
+          subject="subject"
+          description="description"
+          time="6pm"
+        />
+        <EmailRow
+          title="Title"
+          subject="subject"
+          description="descriptionsdfsfdfdfdfdfdfdfdfsdfsdfsdfsdfsdfsdfsdfsdfsdfsdfsdfsdfsdfsdfsdfsdfsdfsdfsd"
+          time="6pm"
+        />
       </div>
     </div>
   );
