99371921e148feaa5ab13e9d15140d9232895215
Armendd <ahmeti.armend@gmail.com>
2021-01-06 20:55:39 +0100

    :bookmark: Show email content from state

--- src/Mail.js
+++ src/Mail.js
@@ -14,9 +14,13 @@ import MoreVertIcon from "@material-ui/icons/MoreVert";
 import UnfoldMoreIcon from "@material-ui/icons/UnfoldMore";
 import PrintIcon from "@material-ui/icons/Print";
 import ExitToAppIcon from "@material-ui/icons/ExitToApp";
+import { useSelector } from "react-redux";
+import { selectOpenMail } from "./features/mailSlice";
+
 
 function Mail() {
   const history = useHistory();
+  const selectedMail = useSelector(selectOpenMail);
 
   return (
     <div className="mail">
@@ -75,13 +79,13 @@ function Mail() {
 
       <div className="mail__body">
         <div className="mail__bodyHeader">
-          <h2>Subject</h2>
+          <h2>{selectedMail?.subject}</h2>
           <LabelImportantIcon className="mail__important" />
-          <p>Title</p>
-          <p className="mail__time">9pm</p>
+          <p>{selectedMail?.title}</p>
+          <p className="mail__time">{selectedMail?.time}</p>
         </div>
         <div className="mail__message">
-          <p>Message</p>
+          <p>{selectedMail?.description}</p>
         </div>
       </div>
     </div>
